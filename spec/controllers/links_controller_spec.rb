709af22339a18c10fb5f33cccbf07a43470579dc
Clinton R. Nixon <crnixon@gmail.com>
2008-12-13 11:14:35 -0500

    updated tests to pass for 0.7.0

--- spec/controllers/links_controller_spec.rb
+++ spec/controllers/links_controller_spec.rb
@@ -1,10 +1,10 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
-describe LinksController do
+describe Admin::LinksController do
 
   #Delete this example and add some real ones
   it "should use LinksController" do
-    controller.should be_an_instance_of(LinksController)
+    controller.should be_an_instance_of(Admin::LinksController)
   end
 
 end
